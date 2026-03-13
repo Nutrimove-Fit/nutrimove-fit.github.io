e72691eebbfd806143ecc784789d1eaefa1d1bdc
mbalde1234 <mbalde1234@users.noreply.github.com>
2026-03-13 10:30:46 +0000

    Deploy Flutter app from JBS@8be72031d5dceba53de65cf66b71ee1b68b533e5 mbalde1234/JBS@8be72031d5dceba53de65cf66b71ee1b68b533e5

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "577816202"
+    serviceWorkerVersion: "1583989170"
   }
 });
